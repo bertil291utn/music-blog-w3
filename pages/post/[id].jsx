abb51b5e0a74b20d3e0375b3f7fcb36fdec4013e
Bertil Tandayamo <bertilt4@gmail.com>
2022-03-13 17:10:13 -0500

    fetch post from graphql server

--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -1,123 +1,112 @@
 /* pages/post/[id].js */
-import ReactMarkdown from 'react-markdown'
-import { useContext } from 'react'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
-import { css } from '@emotion/css'
-import { ethers } from 'ethers'
-import { AccountContext } from '../../context'
+import ReactMarkdown from 'react-markdown';
+import { useContext } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { css } from '@emotion/css';
+import { ethers } from 'ethers';
+import { AccountContext } from '../../context';
 
 /* import contract and owner addresses */
-import {
-  contractAddress, ownerAddress
-} from '../../config'
-import Blog from '../../artifacts/contracts/Blog.sol/Blog.json'
+import { contractAddress, ownerAddress } from '../../config';
+import Blog from '../../artifacts/contracts/Blog.sol/Blog.json';
+import getPosts from '../../data/getPosts';
 
-const ipfsURI = 'https://ipfs.io/ipfs/'
+const ipfsURI = 'https://ipfs.io/ipfs/';
 
 export default function Post({ post }) {
-  const account = useContext(AccountContext)
-  const router = useRouter()
-  const { id } = router.query
+  const account = useContext(AccountContext);
+  const router = useRouter();
+  const { id } = router.query;
 
   if (router.isFallback) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   return (
     <div>
-      {
-        post && (
-          <div className={container}>
-            {
-              /* if the owner is the user, render an edit button */
-              ownerAddress === account && (
-                <div className={editPost}>
-                  <Link href={`/edit-post/${id}`}>
-                    <a>
-                      Edit post
-                    </a>
-                  </Link>
-                </div>
-              )
-            }
-            {
-              /* if the post has a cover image, render it */
-              post.coverImage && (
-                <img
-                  src={post.coverImage}
-                  className={coverImageStyle}
-                />
-              )
-            }
-            <h1>{post.title}</h1>
-            <div className={contentContainer}>
-              <ReactMarkdown>{post.content}</ReactMarkdown>
-            </div>
+      {post && (
+        <div className={container}>
+          {
+            /* if the owner is the user, render an edit button */
+            ownerAddress === account && (
+              <div className={editPost}>
+                <Link href={`/edit-post/${id}`}>
+                  <a>Edit post</a>
+                </Link>
+              </div>
+            )
+          }
+          {
+            /* if the post has a cover image, render it */
+            post.coverImage && (
+              <img src={post.coverImage} className={coverImageStyle} />
+            )
+          }
+          <h1>{post.title}</h1>
+          <div className={contentContainer}>
+            <ReactMarkdown>{post.content}</ReactMarkdown>
           </div>
-        )
-      }
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  /* here we fetch the posts from the network */
-  let provider
-  if (process.env.ENVIRONMENT === 'local') {
-    provider = new ethers.providers.JsonRpcProvider()
-  } else if (process.env.ENVIRONMENT === 'testnet') {
-    provider = new ethers.providers.JsonRpcProvider('https://matic-mumbai.chainstacklabs.com')
-  } else {
-    provider = new ethers.providers.JsonRpcProvider('https://polygon-rpc.com/')
+  const tokensQuery = `
+  query  {
+    posts {
+      id
+      postContent
+      published
+      title
+      contentHash
+    }
   }
+`;
 
-  const contract = new ethers.Contract(contractAddress, Blog.abi, provider)
-  const data = await contract.fetchPosts()
-console.log('data',data)
-  /* then we map over the posts and create a params object passing */
-  /* the id property to getStaticProps which will run for ever post */
-  /* in the array and generate a new page */
-  const paths = data.map(d => ({ params: { id: d.content } }))
+  const posts = await getPosts(tokensQuery);
+  const paths = posts.map((d) => ({ params: { id: d.contentHash } }));
 
   return {
     paths,
-    fallback: true
-  }
+    fallback: true,
+  };
 }
 
 export async function getStaticProps({ params }) {
   /* using the id property passed in through the params object */
   /* we can us it to fetch the data from IPFS and pass the */
   /* post data into the page as props */
-  const { id } = params
-  const ipfsUrl = `${ipfsURI}/${id}`
-  const response = await fetch(ipfsUrl)
-  const data = await response.json()
-  if(data.coverImage) {
-    let coverImage = `${ipfsURI}/${data.coverImage}`
-    data.coverImage = coverImage
+  const { id } = params;
+  const ipfsUrl = `${ipfsURI}/${id}`;
+  const response = await fetch(ipfsUrl);
+  const data = await response.json();
+  if (data.coverImage) {
+    let coverImage = `${ipfsURI}/${data.coverImage}`;
+    data.coverImage = coverImage;
   }
 
   return {
     props: {
-      post: data
+      post: data,
     },
-  }
+  };
 }
 
 const editPost = css`
   margin: 20px 0px;
-`
+`;
 
 const coverImageStyle = css`
   width: 900px;
-`
+`;
 
 const container = css`
   width: 900px;
   margin: 0 auto;
-`
+`;
 
 const contentContainer = css`
   margin-top: 60px;
@@ -127,4 +116,4 @@ const contentContainer = css`
   & img {
     max-width: 900px;
   }
-`
+`;
